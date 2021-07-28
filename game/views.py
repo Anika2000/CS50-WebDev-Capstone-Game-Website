0f567784f9ee80104a7a5af3804fdd9916ef686e
Anika Sultana <anikasultana2000@gmail.com>
2021-07-27 22:08:16 -0400

    Add files via upload

--- game/views.py
+++ game/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from .models import User, GameInfo
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_protect
+from .models import User, GameInfo, Follower, ProfileInfo, Message
 from django.contrib.auth import authenticate, login, logout
 from django.http.response import HttpResponseRedirect
 from django.urls import reverse
@@ -50,7 +52,7 @@ def login_view(request):
             return HttpResponseRedirect(reverse("index"))
         else:
             return render(request, "game/inherit.html", {
-                "message": "Incorrect Login"
+                "message": "Incorrect Login. Try Again"
             })
     return render(request, "game/login.html")
 
@@ -93,6 +95,113 @@ def gameProfile(request, id):
         search_game.save()
         return HttpResponse(status=204)
     return JsonResponse({"error" : "Must be a GET request"}, status=400)
-    
 
-    
+
+def convertToString(string_list): 
+    str_list = " "
+    return (str_list.join(string_list))
+
+@csrf_exempt
+@login_required
+def userProfile(request, name): 
+    try: 
+        search_user = User.objects.get(username=name)
+    except: 
+        return JsonResponse({"error" : "Sorry User does not exist"}, status=400)
+    player_name = search_user.username
+    gamesPlayed = GameInfo.objects.filter(player=search_user).count()
+    followers = search_user.users_followers.all().count()
+    try: 
+        followings = Follower.objects.get(follower=search_user)
+        followings_count = followings.following.all().count()
+    except: 
+        followings_count = 0
+    won_games = GameInfo.objects.filter(player=search_user, win=True)
+    won_games_count = won_games.count()
+    highlight_games_counter = []
+    for game in won_games: 
+        highlight_games_counter.append(1)
+    highlight_games_counter_string = "ERROR"
+    profile = ProfileInfo(player_name=player_name, followers=followers, followings=followings_count, played_games=gamesPlayed, 
+    won_games=won_games_count, highlight_games_counter=highlight_games_counter_string)
+    return JsonResponse(profile.serialize())
+
+@csrf_exempt
+@login_required
+def user(request, name): 
+    try: 
+        search_user = User.objects.get(username=name)
+    except: 
+       return render(request, "game/false.html", {
+           "name" : name
+       })
+    if Follower.objects.filter(follower=request.user, following=search_user).exists(): 
+        follower = True
+    else: 
+        follower = False
+    return render(request, "game/profile.html", {
+        "name" : search_user.username, 
+        "follower" : follower
+    })
+
+
+def follow(request, name): 
+    if request.method == "POST":
+        try: 
+            follower = Follower.objects.get(follower=request.user, follower_name=request.user.username) 
+        except: 
+            follower = Follower.objects.create(follower=request.user, follower_name=request.user.username)
+        wanting_to_follow = User.objects.get(username=name)
+        follower.following.add(wanting_to_follow)
+        return HttpResponseRedirect(reverse("index"))
+
+def unfollow(request, name): 
+    if request.method == "POST": 
+        follower_user = Follower.objects.get(follower=request.user, follower_name=request.user.username)
+        wanting_to_unfollow = User.objects.get(username=name)
+        follower_user.following.remove(wanting_to_unfollow)
+        return HttpResponseRedirect(reverse("index"))
+
+@csrf_exempt
+def chatSend(request): 
+    if request.method == 'POST': 
+        data = json.loads(request.body)
+        sender = request.user
+        sender_name = request.user.username
+        receiver_name = data.get("receiver_name" ,"")
+        chat_message = data.get("message_text", "")
+        try: 
+            receiver = User.objects.get(username=receiver_name)
+        except: 
+            return JsonResponse({"error": "No Username of such found. Try Again"}, status=400)
+        new_message = Message(sender=sender, receiver=receiver, sender_name=sender_name, receiver_name=receiver_name, message_text=chat_message)
+        new_message.save()
+        return JsonResponse({"success": "Message was sent successfully"}, status=201)
+    return JsonResponse({"error": "POST request required."}, status=400)
+
+
+def allMessages(request): 
+    receiver = request.user
+    receiver_name = request.user.username 
+    messages = Message.objects.filter(receiver=receiver, receiver_name=receiver_name)
+    messages = messages.order_by("-timestamp").all()
+    return JsonResponse([message.serialize() for message in messages], safe=False)
+
+
+def wonGames(request, name): 
+    try: 
+        player = User.objects.get(username=name)
+    except: 
+        return JsonResponse({"error": "Incorrect User."}, status=400)
+    games = GameInfo.objects.filter(player=player, win=True)
+    return JsonResponse([game.serialize() for game in games], safe=False)
+
+
+def lostGames(request, name): 
+    try: 
+        player = User.objects.get(username=name)
+    except: 
+        return JsonResponse({"error": "Incorrect User."}, status=400)
+    games = GameInfo.objects.filter(player=player, win=False)
+    return JsonResponse([game.serialize() for game in games], safe=False)
+
